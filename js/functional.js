23541ee6f1a0da2e3db295f2969526b1639e03ea
silvaan <s.siilvan@yahoo.com>
2022-02-18 11:45:27 +0600

    A method for update Total balance

--- js/functional.js
+++ js/functional.js
@@ -15,14 +15,31 @@ function updateValue(fieldId, amount) {
     const newTotal = previousTotal + amount;
     totalFiled.innerText = newTotal;
 }
+// function for upadate Balance
+function updateBalance(amount, isAdding) {
+    const balanceTag = document.getElementById('balance-total');
+    const balanceInText = balanceTag.innerText;
+    const previousBalance = parseFloat(balanceInText);
+    let newBalance;
+    if (isAdding) {
+        newBalance = previousBalance + amount;
+    }
+    else {
+        newBalance = previousBalance - amount;
+    }
+    balanceTag.innerText = newBalance;
+}
+
 // handle Deposit Button
 document.getElementById('deposit-button').addEventListener('click', function () {
     const amount = getInputValue('deposit-input');
     updateValue('deposit-total', amount);
+    updateBalance(amount, true);
 })
 
 // handle withdraw Button
 document.getElementById('withdraw-button').addEventListener('click', function () {
     const amount = getInputValue('withdraw-input');
     updateValue('withdraw-total', amount);
+    updateBalance(amount, false);
 })
