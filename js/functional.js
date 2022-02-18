b178070f3aa8d4743b20e9e408d476761dfed972
silvaan <s.siilvan@yahoo.com>
2022-02-18 11:19:21 +0600

    Function For Update Value

--- js/functional.js
+++ js/functional.js
@@ -7,7 +7,17 @@ function getInputValue(inputFieldId) {
     return value;
 }
 
+// function for update total 
+function updateValue(fieldId, amount) {
+    const totalFiled = document.getElementById(fieldId);
+    const totalInText = totalFiled.innerText;
+    const previousTotal = parseFloat(totalInText);
+    const newTotal = previousTotal + amount;
+    totalFiled.innerText = newTotal;
+}
+
 document.getElementById('deposit-button').addEventListener('click', function () {
     const amount = getInputValue('deposit-input');
     console.log(amount);
+    updateValue('deposit-total', amount);
 })
